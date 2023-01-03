aa091a7c2cc5fcb5ce908336d302b5e44b75dc3b
oneJS-dev <onejs.dev@gmail.com>
2023-01-03 17:59:25 +0100

    new version

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# template
-App template. Do not use on its own. Use 'npx @onejs-dev/create-app' command instead.
+
+<div align="center">
+  <img alt="oneJS" src="https://user-images.githubusercontent.com/98762998/210245869-21fdfcf0-4aff-41e3-a4c0-081505016f6d.svg" width="80px">
+</div>
+<!-- Title -->
+<h1 align="center">
+  <a href="https://oneJS.dev/">
+    oneJS
+  </a>
+</h1>
+
+<!-- Slogan -->
+<p align="center">
+  The <strong>one</strong> framework you need
+</p>
+
+<!-- Chip Cards -->
+<p align="center">
+  <!-- License -->
+  <a href="https://github.com/onejs-dev/onejs-dev/blob/HEAD/LICENSE">
+    <img src="https://img.shields.io/badge/license-MIT-blue.svg" alt="oneJS is released under the MIT license."/>
+  </a>
+  <!-- Npm Version -->
+  <a href="https://www.npmjs.org/package/@onejs-dev/core">
+    <img src="https://img.shields.io/npm/v/@onejs-dev/core?color=brightgreen&label=npm%20package" alt="Current npm package version." />
+  </a>
+  <!-- PRs -->
+  <a href="https://reactnative.dev/docs/contributing">
+    <img src="https://img.shields.io/badge/PRs-welcome-brightgreen.svg" alt="PRs welcome!" />
+  </a>
+</p>
+
+A **functional**, **JavaScript only**, **cross platform** framework.
+<br><br>
+oneJS is a next generation, open source JavaScript framework for building modern apps for web and mobile from a single codebase. oneJS is not owned by any Tech Giant; it is an independent project developed by contributors from all around the world. oneJS is based on three principles:
+
+- **Functional:** Predictable, code efficient and easily testable. Pure functions are the building blocks of oneJS. Fall in love with the functional programming paradigm ❤️.
+- **JS Only. Really:** You can write your entire app in pure Vanilla JS or Typescript. That's right, not a single line of HTML, CSS or JSX needed. Can you believe it?
+- **Cross Platform:** Write the same code for web and native apps. Simplicity is our true cross platform weapon; reduce your effort and multiply your reach! Powered by [React][react] and [React Native's][rn] new architecture.
+
+[react]: https://reactjs.org/
+[rn]: https://reactnative.dev/
+
+## 🔒 Internal Repository
+
+This is an internal repo used by the app creator command: ```$ npx @onejs-dev/create-app```. You should not require to directly download or manipulate this repository.
+
+<div align="center">
+  <h1></h1>
+  <h1>
+    <img alt="oneJS" src="https://user-images.githubusercontent.com/98762998/210245537-180314c5-51b1-4b01-b3b8-d02d50b77d73.svg" width="60px">
+  </h1>
+</div>
--- src/App.js
+++ src/App.js
@@ -1,88 +1,146 @@
 /* Imports: Required modules to setup the app =================================================== */
-import {App, os, read, update, readFlavor, readStyle} from '@onejs-dev/core';
-import {Text, View, Input, Icon} from '@onejs-dev/components';
-import homeIcon from '../assets/icons/oneJS.svg';
-import aboutIcon from '../assets/icons/book.svg';
+import {app, os, read, update, readFlavor, readStyle} from '@onejs-dev/core';
+import {Text, View, Input} from '@onejs-dev/components';
+import {icons} from '@onejs-dev/icons';
 
-/* State: Any variable subject to change needs to be part of the state ========================== */
+/* State: Variable configuration definition ===================================================== */
 const state = {
-  /* Routes: The source for their value is the url. True when matches false otherwise */
-  home:       {source: {url: '/'}}, 
-  about:      {source: {url: '/about'}},
+    /* Routes: The source for their value is the url. 'true' when matches, 'false' otherwise */
+    home: {source: {url: '/'}},
+    about: {source: {url: '/about'}},
 
-  /* Standard variables: The value provided is their default/initial value */
-  fontFamily: 'Avenir Next',
-  opacity:    1,
+    /* Standard variables: The value provided is their default/initial value */
+    fontFamily: 'Avenir Next',
+    opacity: 1,
 
-  /* Stored variable: It is stored and retrieved from local storage using the key 'greeting' */
-  greeting:   {default: 'World', source: {local: 'greeting'}, storage: {local: 'greeting'}},
+    /* Stored variable: It is stored and retrieved from local storage using the key 'greeting' */
+    greeting: {
+        default: 'World',
+        source: {local: 'greeting'},
+        storage: {local: 'greeting'}
+    }
 };
 
-/* Fonts: Optional, import custom fonts ========================================================= */
-const font = { 
-  'Avenir Next': require('../assets/fonts/AvenirNext.otf'),
-  'Domine': require('../assets/fonts/Domine.ttf'),
+/* Fonts: Import custom fonts =================================================================== */
+const font = {
+    'Avenir Next': require('../assets/fonts/AvenirNext.otf'),
+    Domine: require('../assets/fonts/Domine.ttf')
 };
 
 /* Style: Customize the look and feel =========================================================== */
 const style = {
-  page:          {backgroundColor: '#F1F1F1', position: 'absolute', height: '100%', width: '100%'},
-  navbar:        {position: 'absolute', bottom: 0, height: 80, width: '100%', 
-                  backgroundColor: '#fff'},
-  bigSpaceTop:   {marginTop: 80},
-  smallSpaceTop: {marginTop: 20}
+    page: {
+        backgroundColor: '#F1F1F1',
+        position: 'absolute',
+        width: '100%',
+        minHeight: '100vh'
+    },
+    navbar: {
+        position: 'fixed',
+        bottom: 0,
+        height: 80,
+        width: '100%',
+        backgroundColor: '#fff'
+    }
 };
 
-/* Template function: Returns the structure to be rendered ====================================== */
-const template = () => {
-  return [
-  /* Home page */
-  View({visible: read('home'), animation: {visible: ['fade-in', 'fade-out']}, 
-        self: {expand: 1}, content: {h: 'center', v: 'top', direction: 'column'},
-        style: readStyle('page')})([
-    /* Greeting text */
-    Text({flavor: readFlavor('title', 'primary'), style: {opacity: read('opacity'), 
-          fontFamily: read('fontFamily'), marginTop: 80}})('Hello ' + read('greeting') + '!'),
+/* App function: Returns the structure to be rendered =========================================== */
+const App = () => {
+    return [
+        /* Home page */
+        View({
+            visible: read('home'),
+            animation: {visible: ['fade-in', 'fade-out']},
+            self: {expand: 1},
+            content: {h: 'center', v: 'center', direction: 'column', gap: 10},
+            style: readStyle('page')
+        })([
+            /* Greeting text */
+            Text({
+                flavor: readFlavor('title', 'primary'),
+                style: {
+                    opacity: read('opacity'),
+                    fontFamily: read('fontFamily')
+                }
+            })('Hello ' + read('greeting') + '!'),
 
-    /* Text input to update greeting */
-    Text({style: readStyle('smallSpaceTop')})('Greeting text: '),
-    Input({type: 'text', onChange: update('greeting'), value: read('greeting')}),
+            /* Text input to update greeting */
+            Text()('Greeting text: '),
+            Input({
+                type: 'text',
+                onChange: update('greeting'),
+                value: read('greeting')
+            }),
 
-    /* List input to update font family */
-    Text({style: readStyle('smallSpaceTop')})('Font family: '),
-    Input({type: 'list', onChange: update('fontFamily'), value: read('fontFamily'), 
-           options: ['Avenir Next', 'Domine']}),
+            /* List input to update font family */
+            Text()('Font family: '),
+            Input({
+                type: 'list',
+                onChange: update('fontFamily'),
+                value: read('fontFamily'),
+                options: ['Avenir Next', 'Domine']
+            }),
 
-    /* Range input to update opacity */
-    Text({style: readStyle('smallSpaceTop')})('Font opacity: '),
-    Input({type: 'range', min: 0, max: 1, step: 0.1, value: read('opacity'), 
-           onChange: update('opacity')}),
-  ]),
+            /* Range input to update opacity */
+            Text()('Font opacity: '),
+            Input({
+                type: 'range',
+                min: 0,
+                max: 1,
+                step: 0.1,
+                value: read('opacity'),
+                onChange: update('opacity'),
+                style: {marginBottom: 100}
+            })
+        ]),
 
-  /* About page */
-  View({visible: read('about'), animation: {visible: ['fade-in', 'fade-out']}, 
-        self: {expand: 1}, content: {h: 'center', v: 'top', direction: 'column'},
-        style: readStyle('page')})([
-    /* About page text */
-    Text({flavor: readFlavor('header'), 
-          style: readStyle('bigSpaceTop')})('Developed with ❤️ by oneJS'),
-    Text({style: readStyle('smallSpaceTop')})('Running on ' + os)
-  ]),
+        /* About page */
+        View({
+            visible: read('about'),
+            animation: {visible: ['fade-in', 'fade-out']},
+            self: {expand: 1},
+            content: {h: 'center', v: 'center', direction: 'column', gap: 20},
+            style: readStyle('page')
+        })([
+            /* About page text */
+            Text({flavor: readFlavor('section')})('Developed with ❤️ by oneJS'),
+            Text()('Running on ' + os)
+        ]),
 
-  /* Navbar */
-  View({content: {h: 'distribute', v: 'center'}, style: readStyle('navbar')})([
-    /* Home navbar item */
-    Input({type: 'button', icon: homeIcon, title: 'Home', url: '/', 
-           content: {h: 'center', v: 'center', direction: 'column'}, 
-           flavor: readFlavor((read('home') ? 'selected' : 'unselected'), 'flat')}),
-    /* About navbar item */
-    Input({type: 'button', icon: aboutIcon, title: 'About', url: '/about',
-          content: {h: 'center', v: 'center', direction: 'column'}, 
-          flavor: readFlavor((read('about') ? 'selected' : 'unselected'), 'flat')}),
-  ])];
-}
+        /* Navbar */
+        View({
+            content: {h: 'distribute', v: 'center'},
+            style: readStyle('navbar')
+        })([
+            /* Home navbar item */
+            Input({
+                type: 'button',
+                icon: icons['oneJS'],
+                title: 'Home',
+                url: '/',
+                content: {h: 'center', v: 'center', direction: 'column'},
+                flavor: readFlavor(read('home') ? 'selected' : 'unselected', 'flat')
+            }),
+            /* About navbar item */
+            Input({
+                type: 'button',
+                icon: icons['book'],
+                title: 'About',
+                url: '/about',
+                content: {h: 'center', v: 'center', direction: 'column'},
+                flavor: readFlavor(read('about') ? 'selected' : 'unselected', 'flat')
+            })
+        ])
+    ];
+};
 
-/* App: Run the app with the parameters provided ================================================ */
-App({theme: 'oneJS', state: state, font: font, style: style})(template);
+/* App: Render the app with the parameters provided ============================================== */
+app({
+    component: App,
+    theme: {preset: 'oneJS'},
+    state: state,
+    font: font,
+    style: style
+});
 
-/* Learn: Refer to the docs section to learn all about oneJS: https://onejs.dev/docs============= */
+/* Learn: Refer to the 'learn' section to learn all about oneJS: https://onejs.dev/learn ======== */
